b5bd7c79900d6c396eb373d61eed271bb0d752b9
Jacob Salomonsen <jiekebo@gmail.com>
2011-03-14 21:43:15 +0100

    Added Rafael's code

--- fs/fat32.c
+++ fs/fat32.c
@@ -11,48 +11,537 @@
 #include "drivers/gbd.h"
 #include "fs/vfs.h"
 #include "fs/fat32.h"
+#include "kernel/config.h"
 
-// Table for Secto size and offset using the sec_t struct, all numbers are bytes.
-
-sec_t sector_offset[] = {
-		{0,3},
-		{3,8},
-		{11,2},
-		{13,1},
-		{14,2},
-		{16,1},
-		{17,2},
-		{19,2},
-		{21,2},
-		{22,1},
-		{24,2},
-		{26,2},
-		{28,2},
-		{32,4},
-		{36,4},
-		{40,2},
-		{42,2},
-		{44,4},
-		{48,2},
-		{50,2},
-		{52,12},
-		{64,2},
-		{65,1},
-		{66,1},
-		{67,4},
-		{71,11},
-		{82,8},
-};
-
-int mask(int size) {
+// Table for Sector size and offset using the sec_t struct, all numbers are bytes.
 
 
-
-	return 1 << (4*size);
+uint32_t low_to_big32(uint32_t x) {
+	uint8_t *p = (uint8_t *) &x;
+	return p[0] + (p[1] << 8) + (p[2] << 16) + (p[3] << 24);
 }
-fs_t * fat32_init(gbd_t *disk) {
 
-
-
-
+uint16_t low_to_big16(uint16_t x) {
+	uint8_t *p = (uint8_t *) &x;
+	return p[0] + (p[1] << 8);
 }
+
+uint32_t big_to_low32(uint32_t x) {
+	uint8_t *p = (uint8_t *) &x;
+	return p[3] + (p[2] << 8) + (p[1] << 16) + (p[0] << 24);
+}
+
+uint16_t big_to_low16(uint16_t x) {
+	uint8_t *p = (uint8_t *) &x;
+	return p[3] + (p[2] << 8);
+}
+
+#define DATA_GET(type, addr, offset) (*(type*)(((uint8_t*)addr)+offset))
+#define DATA_SET(type, addr, offset, data) memoryset(((uint8_t*)addr) + offset, data, sizeof(type))
+#define IS_VALID_FILEID(fid) (fid < CONFIG_MAX_OPEN_FILES+3 && fid >= 2)
+
+typedef struct {
+	uint32_t fat_begin_lba;
+	uint32_t cluster_begin_lba;
+	uint32_t sectors_per_cluster;
+	uint32_t root_dir_first_cluster;
+
+	semaphore_t *lock;
+
+	directory_t *filetable[CONFIG_MAX_OPEN_FILES];
+
+	gbd_t *disk;
+} fat32_t;
+
+inline uint32_t cluster2block(fat32_t *fat, uint32_t cluster) {
+	return fat->cluster_begin_lba + (cluster - 2) * fat->sectors_per_cluster;
+}
+
+uint32_t fat32_fat_lookup(fat32_t *fat, uint32_t cluster) {
+	uint32_t addr;
+	int r;
+	gbd_request_t req;
+	uint32_t retval;
+
+	addr = pagepool_get_phys_page();
+	if (addr == 0) {
+		kprintf("fat32_fat_lookup: could not allocate memory.\n");
+		return -1;
+	}
+	addr = ADDR_PHYS_TO_KERNEL(addr);
+
+	req.block = fat->fat_begin_lba + (cluster / (512 / 32));
+
+	req.sem = NULL;
+	req.buf = ADDR_KERNEL_TO_PHYS(addr); /* disk needs physical addr */
+	r = fat->disk->read_block(fat->disk, &req);
+	if (r == 0) {
+		pagepool_free_phys_page(ADDR_KERNEL_TO_PHYS(addr));
+		kprintf(
+				"fat32_fat_lookup: Error during disk read. Initialization failed.\n");
+		return -1;
+	}
+
+	retval = DATA_GET(uint32_t, addr, cluster % (512/32));
+	return retval;
+}
+
+int fat32_fat_cleanup(fat32_t *fat, uint32_t cluster) {
+	uint32_t addr;
+	int r;
+	gbd_request_t req;
+	uint32_t tmp;
+
+	addr = pagepool_get_phys_page();
+	if (addr == 0) {
+		pagepool_free_phys_page(ADDR_KERNEL_TO_PHYS(addr));
+		kprintf("fat32_fat_cleanup: couldnt allocate page\n");
+		return -1;
+	}
+	addr = ADDR_PHYS_TO_KERNEL(addr);
+
+	do {
+		req.block = fat->fat_begin_lba + (cluster / (512 / 32));
+
+		req.sem = NULL;
+		req.buf = ADDR_KERNEL_TO_PHYS(addr); /* disk needs physical addr */
+		r = fat->disk->read_block(fat->disk, &req);
+		if (r == 0) {
+			pagepool_free_phys_page(ADDR_KERNEL_TO_PHYS(addr));
+			kprintf("fat32_fat_cleanup: Error during disk read.\n");
+			return -1;
+		}
+
+		tmp = DATA_GET(uint32_t, addr, cluster % (512/32));
+		DATA_SET(uint32_t, addr, cluster % (512/32), 0);
+
+		r = fat->disk->write_block(fat->disk, &req);
+		if (r == 0) {
+			pagepool_free_phys_page(ADDR_KERNEL_TO_PHYS(addr));
+			kprintf("fat32_fat_cleanup: Error during disk write.\n");
+			return -1;
+		}
+
+		cluster = tmp;
+	} while (cluster != 0xFFFFFFFF);
+
+	return 0;
+}
+
+int next_dir_entry(fat32_t *fat, directory_t *entry) {
+	uint32_t addr;
+	int r;
+	gbd_request_t req;
+
+	addr = pagepool_get_phys_page();
+	if (addr == 0) {
+		kprintf("fat32_next_dir_entry: could not allocate memory.\n");
+		return -1;
+	}
+	addr = ADDR_PHYS_TO_KERNEL(addr);
+
+	semaphore_P(fat->lock);
+
+	req.block = cluster2block(fat, entry->cluster) + entry->sector;
+	kprintf("block: %p\n", req.block);
+	req.sem = NULL;
+	req.buf = ADDR_KERNEL_TO_PHYS(addr);
+	r = fat->disk->read_block(fat->disk, &req);
+	if (r == 0) {
+		semaphore_V(fat->lock);
+		pagepool_free_phys_page(ADDR_KERNEL_TO_PHYS(addr));
+		kprintf("fat32_next_dir_entry: Error during disk read. FAIL\n");
+		return -1;
+	}
+
+	do {
+		kprintf("entry: %d\n", entry->entry);
+		if ((++entry->entry) >= (512 / 32)) {
+			entry->entry = 0;
+			entry->sector++;
+
+			if (entry->sector >= fat->sectors_per_cluster) {
+				entry->sector = 0;
+				entry->cluster = fat32_fat_lookup(fat, entry->cluster);
+			}
+
+			req.block = cluster2block(fat, entry->cluster) + entry->sector;
+			req.sem = NULL;
+			req.buf = ADDR_KERNEL_TO_PHYS(addr);
+			r = fat->disk->read_block(fat->disk, &req);
+			if (r == 0) {
+				semaphore_V(fat->lock);
+				pagepool_free_phys_page(ADDR_KERNEL_TO_PHYS(addr));
+				kprintf("fat32_next_dir_entry: Error during disk read. FAIL\n");
+				return -1;
+			}
+		}
+
+		stringcopy(entry->name, (char *) (addr + (entry->entry * 32)), 11);
+		if (entry->name[0] == 0)
+			return -1;
+
+		entry->attr = DATA_GET(uint32_t, addr, (entry->entry * 32) + 0x0B);
+	} while ((uint8_t) entry->name[0] == FAT32_DIR_ENTRY_UNUSED || (entry->attr
+			& 0x1111));
+
+	entry->high_cluster
+			= DATA_GET(uint16_t *, addr, (entry->entry * 32) + 0x14);
+	entry->low_cluster = DATA_GET(uint16_t *, addr, (entry->entry * 32) + 0x1A);
+	entry->size = DATA_GET(uint32_t, addr, (entry->entry * 32) + 0x1C);
+
+	semaphore_V(fat->lock);
+
+	pagepool_free_phys_page(ADDR_KERNEL_TO_PHYS(addr));
+
+	return 0;
+}
+
+int search_dir(fat32_t *fat, directory_t *entry, int(*pred)(directory_t *entry)) {
+	while (next_dir_entry(fat, entry) >= 0) {
+		if (pred(entry)) {
+			return VFS_OK;
+		}
+	}
+
+	return VFS_NOT_FOUND;
+}
+
+int search_dir_by_filename(fat32_t *fat, directory_t *entry, const char *name) {
+	while (next_dir_entry(fat, entry) >= 0) {
+		if (stringcmp(entry->name, name) == 0) {
+			return VFS_OK;
+		}
+	}
+
+	return VFS_NOT_FOUND;
+}
+
+int is_volume_id(directory_t *entry) {
+	return (entry->attr & 8);
+}
+
+fs_t *fat32_init(gbd_t *disk) {
+	uint32_t addr;
+	fs_t *fs;
+	semaphore_t *sem;
+	gbd_request_t req;
+	int r;
+	int reserved_sector_count;
+	int secs_per_fat;
+	int num_fats;
+	fat32_t *fat;
+	directory_t direntry;
+
+	sem = semaphore_create(1);
+	if (sem == NULL) {
+		kprintf("fat32_init: could not create a new semaphore.\n");
+		return NULL;
+	}
+
+	addr = pagepool_get_phys_page();
+	if (addr == 0) {
+		semaphore_destroy(sem);
+		kprintf("fat32_init: could not allocate memory.\n");
+		return NULL;
+	}
+	addr = ADDR_PHYS_TO_KERNEL(addr);
+
+	req.block = 0;
+	req.sem = NULL;
+	req.buf = ADDR_KERNEL_TO_PHYS(addr); /* disk needs physical addr */
+	r = disk->read_block(disk, &req);
+	if (r == 0) {
+		semaphore_destroy(sem);
+		pagepool_free_phys_page(ADDR_KERNEL_TO_PHYS(addr));
+		kprintf("fat32_init: Error during disk read. Initialization failed.\n");
+		return NULL;
+	}
+
+	// check that file system is FAT32
+	if (low_to_big16(DATA_GET(uint16_t, addr, FAT32_SIG_OFFSET))
+			!= FAT32_SIGNATURE) {
+		semaphore_destroy(sem);
+		pagepool_free_phys_page(ADDR_KERNEL_TO_PHYS(addr));
+		return NULL;
+	}
+
+	fs = (fs_t *) addr;
+	fat = (fat32_t *) (addr + sizeof(fs_t));
+	fs->internal = (void *) fat;
+
+	fat->lock = sem;
+	fat->disk = disk;
+
+	// read partition header
+	reserved_sector_count = low_to_big16(
+			DATA_GET(uint16_t, addr, FAT32_RESERVED_SECTOR_COUNT_OFFSET));
+	secs_per_fat = low_to_big32(
+			DATA_GET(uint32_t, addr, FAT32_SECS_PER_FAT_OFFSET));
+	num_fats = DATA_GET(uint8_t, addr, FAT32_NUM_FATS_OFFSET);
+
+	fat->sectors_per_cluster
+			= DATA_GET(uint8_t, addr, FAT32_SECS_PER_CLUS_OFFSET);
+	fat->root_dir_first_cluster = low_to_big32(
+			DATA_GET(uint32_t, addr, FAT32_ROOT_CLUS_OFFSET));
+	fat->fat_begin_lba = FAT32_MBR_SIZE + reserved_sector_count;
+	fat->cluster_begin_lba = FAT32_MBR_SIZE + reserved_sector_count + (num_fats
+			* secs_per_fat);
+
+	memoryset(fat->filetable, 0, sizeof(directory_t *) * CONFIG_MAX_OPEN_FILES);
+
+	direntry.cluster = 2;
+	direntry.sector = 0;
+	direntry.entry = 0;
+	if (search_dir(fat, &direntry, is_volume_id) < 0) {
+		KERNEL_PANIC("Volume label not found\n");
+	}
+
+	fat->filetable[3] = &direntry;
+	fat32_read(fs, 3, fs->volume_name, 16, 0);
+	fat->filetable[3] = NULL;
+
+	fs->unmount = fat32_unmount;
+	fs->open = fat32_open;
+	fs->close = fat32_close;
+	fs->create = fat32_create;
+	fs->remove = fat32_remove;
+	fs->read = fat32_read;
+	fs->write = fat32_write;
+	fs->getfree = fat32_getfree;
+
+	return fs;
+}
+
+int fat32_unmount(fs_t *fs) {
+	fat32_t *fat;
+	fat = (fat32_t *) fs->internal;
+
+	semaphore_P(fat->lock);
+	semaphore_destroy(fat->lock);
+
+	pagepool_free_phys_page(ADDR_KERNEL_TO_PHYS((uint32_t) fs));
+
+	return VFS_OK;
+}
+
+int fat32_open(fs_t *fs, char *filename) {
+
+	fs = fs;
+	filename = filename;
+	/*
+	fat32_t *fat;
+	directory_t *direntry;
+	int i;
+
+	fat = (fat32_t *) fs->internal;
+	direntry = (directory_t*) pagepool_get_phys_page();
+
+	semaphore_P(fat->lock);
+
+	for (i = 0; i < CONFIG_MAX_OPEN_FILES; ++i) {
+		if (fat->filetable[i] != NULL) {
+			direntry = fat->filetable[i];
+			direntry->cluster = 2;
+			direntry->sector = 0;
+			direntry->entry = 0;
+
+			if (search_dir_by_filename(fat, direntry, filename)
+					== VFS_NOT_FOUND) {
+				semaphore_V(fat->lock);
+				return VFS_NOT_FOUND;
+			}
+
+			fat->filetable[i] = direntry;
+			semaphore_V(fat->lock);
+			return i + 3;
+		}
+	}
+
+	// file table is full
+	semaphore_V(fat->lock);
+	return VFS_LIMIT;
+	*/
+	return 0;
+}
+
+int fat32_close(fs_t *fs, int fileid) {
+	fat32_t *fat;
+	fat = (fat32_t *) fs->internal;
+
+	if (!IS_VALID_FILEID(fileid)) {
+		return VFS_ERROR;
+	}
+
+	semaphore_P(fat->lock);
+	fat->filetable[fileid - 3] = NULL;
+	semaphore_V(fat->lock);
+
+	return VFS_OK;
+}
+
+int fat32_create(fs_t *fs, char *filename, int size) {
+	fs = fs;
+	filename = filename;
+	size = size;
+	return 0;
+}
+
+int fat32_remove(fs_t *fs, char *filename) {
+
+	fs = fs;
+	filename = filename;
+	/*
+	fat32_t *fat = (fat32_t *) fs->internal;
+	directory_t direntry;
+	gbd_request_t req;
+	int r;
+	int addr;
+
+	semaphore_P(fat->lock);
+
+	direntry.cluster = 2;
+	direntry.sector = 0;
+	direntry.entry = 0;
+
+	if (search_dir_by_filename(fat, &direntry, filename) == VFS_NOT_FOUND) {
+		semaphore_V(fat->lock);
+		return VFS_NOT_FOUND;
+	}
+
+	addr = pagepool_get_phys_page();
+
+	req.block = (direntry.cluster * fat->sectors_per_cluster) + direntry.sector;
+	req.buf = addr; // TODO: make a buffer to write from
+	req.sem = NULL;
+	r = fat->disk->read_block(fat->disk, &req);
+	if (r == 0) {
+		semaphore_V(fat->lock);
+		pagepool_free_phys_page(addr);
+		return VFS_ERROR;
+	}
+
+	DATA_SET(uint8_t, addr, direntry.entry*32, 0xE5);
+
+	r = fat->disk->write_block(fat->disk, &req);
+	if (r == 0) {
+		semaphore_V(fat->lock);
+		pagepool_free_phys_page(addr);
+		return VFS_ERROR;
+	}
+
+	if (fat32_fat_cleanup(fat, direntry.cluster) < 0) {
+		semaphore_V(fat->lock);
+		pagepool_free_phys_page(addr);
+		return VFS_ERROR;
+	}
+
+	semaphore_V(fat->lock);
+
+	return VFS_OK;
+	*/
+	return 0;
+}
+
+int fat32_read(fs_t *fs, int fileid, void *buffer, int bufsize, int offset) {
+
+	fs = fs;
+	fileid = fileid;
+	buffer = buffer;
+	bufsize = bufsize;
+	offset = offset;
+	/*
+	fat32_t *fat = (fat32_t *) fs->internal;
+	gbd_request_t req;
+	directory_t *direntry;
+	uint32_t rbuf;
+	int r;
+	int read = 0;
+	uint32_t i;
+	uint32_t cluster;
+
+	if (!IS_VALID_FILEID(fileid)) {
+		return VFS_ERROR;
+	}
+
+	semaphore_P(fat->lock);
+
+	if (fat->filetable[fileid - 3] == NULL) {
+		semaphore_V(fat->lock);
+		return VFS_NOT_OPEN;
+	}
+
+	direntry = fat->filetable[fileid - 3];
+	bufsize = MIN(bufsize, ((int)direntry->size) - offset);
+
+		if (bufsize == 0) {
+			semaphore_V(fat->lock);
+			return 0;
+		}
+	direntry = fat->filetable[fileid - 3];
+
+	if (offset < 0 || (uint32_t) offset > direntry->size) {
+		semaphore_V(fat->lock);
+		return VFS_ERROR;
+	}
+
+
+
+	rbuf = pagepool_get_phys_page();
+
+	cluster = (uint32_t) direntry->low_cluster
+			+ ((uint32_t) direntry->high_cluster << 4);
+	req.block = cluster2block(fat, cluster);
+	req.buf = rbuf;
+	req.sem = NULL;
+
+	while (cluster != 0xFFFFFFFF && read < bufsize) {
+		for (i = 0; i < fat->sectors_per_cluster; ++i) {
+			r = fat->disk->read_block(fat->disk, &req);
+
+			if (r == 0) {
+				pagepool_free_phys_page(rbuf);
+				semaphore_V(fat->lock);
+				return VFS_ERROR;
+			}
+
+			memcopy(r, buffer, (uint32_t*) rbuf);
+			read += r;
+
+			req.block += 512;
+		}
+
+		cluster = fat32_fat_lookup(fat, cluster);
+		req.block = cluster2block(fat, cluster);
+	}
+
+	pagepool_free_phys_page(rbuf);
+	semaphore_V(fat->lock);
+	return read; */
+	return 0;
+}
+
+
+int fat32_write(fs_t *fs, int fileid, void *buffer, int datasize, int offset) {
+	fs = fs;
+	fileid = fileid;
+	buffer = buffer;
+	datasize = datasize;
+	offset = offset;
+	/*fat32_t *fat = (fat32_t *) fs->internal;
+	gbd_request_t req;
+	directory_t *direntry;
+	uint32_t rbuf;
+
+
+	uint32_t i;
+	uint32_t cluster;
+*/
+	return 0;
+}
+
+int fat32_getfree(fs_t *fs) {
+	fs = fs;
+	return 0;
+}
+
--- fs/fat32.h
+++ fs/fat32.h
@@ -11,59 +11,42 @@
 #define FAT32_MBR_SIZE 0
 #define FAT32_BLOCK_SIZE 512
 
-
 #include "drivers/gbd.h"
 #include "fs/vfs.h"
 
 fs_t * fat32_init(gbd_t *disk);
 
-int tfs_unmount(fs_t *fs);
-int tfs_open(fs_t *fs, char *filename);
-int tfs_close(fs_t *fs, int fileid);
-int tfs_create(fs_t *fs, char *filename, int size);
-int tfs_remove(fs_t *fs, char *filename);
-int tfs_read(fs_t *fs, int fileid, void *buffer, int bufsize, int offset);
-int tfs_write(fs_t *fs, int fileid, void *buffer, int datasize, int offset);
-int tfs_getfree(fs_t *fs);
+int fat32_unmount(fs_t *fs);
+int fat32_open(fs_t *fs, char *filename);
+int fat32_close(fs_t *fs, int fileid);
+int fat32_create(fs_t *fs, char *filename, int size);
+int fat32_remove(fs_t *fs, char *filename);
+int fat32_read(fs_t *fs, int fileid, void *buffer, int bufsize, int offset);
+int fat32_write(fs_t *fs, int fileid, void *buffer, int datasize, int offset);
+int fat32_getfree(fs_t *fs);
 
-enum Sector_name {
-	BS_jmpBoot,
-	BS_OEMName,
-	BPB_BytsPerSec,
-	BPB_SecPerClus,
-	BPB_RsvdSecCnt,
-	BPB_NumFATs,
-	BPB_RootEntCnt,
-	BPB_TotSec16,
-	BPB_Media,
-	BPB_FATSsz16,
-	BPB_SecPerTrk,
-	BPB_NumHeads,
-	BPB_HiddSec,
-	BPB_TotSec32,
-	BPB_FATSz32,
-	BPB_ExitFlags,
-	BPB_FSVer,
-	BPB_RootClus,
-	BPB_FSInfo,
-	BPB_BkBootSec,
-	BPB_Reserved,
-	BS_DrvNum,
-	BS_Reserved,
-	BS_DrvNum,
-	BS_Reserved1,
-	BS_BootSig,
-	BS_VolID,
-	BS_FIlSysType
-
-};
-
-struct {
-
-	int offset;
-	int size;
+#define FAT32_SIGNATURE 0xAA55
+#define FAT32_DIR_ENTRY_UNUSED 0xE5
+#define FAT32_MBR_SIZE 0
+#define FAT32_RESERVED_SECTOR_COUNT_OFFSET 0x0E
+#define FAT32_NUM_FATS_OFFSET 0x10
+#define FAT32_SECS_PER_FAT_OFFSET 0x24
+#define FAT32_SECS_PER_CLUS_OFFSET 0x0D
+#define FAT32_ROOT_CLUS_OFFSET 0x2C
+#define FAT32_SIG_OFFSET 0x1FE
 
 
-} sec_t;
 
+typedef struct {
+	char name[10];
+	uint32_t attr;
+	uint32_t size;
+	uint32_t cluster;
+	uint32_t sector;
+	uint32_t entry;
+
+	uint16_t *low_cluster;
+	uint16_t *high_cluster;
+
+} directory_t;
 #endif /* FAT32_H_ */
